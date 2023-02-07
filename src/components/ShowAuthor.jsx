6f309f8269daedef6fa0adea14fd343ca98797d1
mojtaba <mojtaba75rs@gmail.com>
2023-02-07 12:28:46 +0330

    Edit the styles of the Show Author component

--- src/components/ShowAuthor.jsx
+++ src/components/ShowAuthor.jsx
@@ -6,23 +6,19 @@ import { selectUserById } from "../reducers/userSlice";
 const ShowAuthor = ({ author, created_at }) => {
     const user = useSelector((state) => selectUserById(state, author));
     return (
-        <div className='flex flex-col items-start space-y-2 space-x-0 sm:flex-row sm:items-start sm:space-x-4 sm:space-y-0'>
+        <div className='flex items-start space-x-4'>
             <img src={user.avatar_path} className="w-14 h-14 rounded-full bg-yellow-500 text-xs text-center" alt={user.name} />
             <div className="space-y-1">
-                <h6 className="text-lg text-teal-900 font-bold tracking-wide">
+                <h6 className="text-base text-teal-900 font-bold tracking-wide">
                     {user.name}
                 </h6>
-                <div className="flex items-center space-x-1">
+                <div className="flex flex-col items-start space-x-1 sm:flex-row sm:items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-teal-900">
                         <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
                         <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clipRule="evenodd" />
                     </svg>
                     <p className="text-sm text-slate-500">
-                        {
-                            // formatDistance(subDays(new Date(created_at).toLocaleDateString("en-US", { month: 'short', day: 'numeric', year: 'numeric' }),3),new Date(),{addSuffix:true})
-                            formatDistance(subDays(new Date(created_at), 2), new Date(), { addSuffix: true })
-                            // formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
-                        }
+                        {formatDistance(subDays(new Date(created_at), 2), new Date(), { addSuffix: true })}
                     </p>
                 </div>
             </div>
